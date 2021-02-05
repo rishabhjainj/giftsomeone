be6ac138922fbef2b39f1e3b31485105bc00de94
Rishabh <jrishabh252@gmail.com>
2021-02-05 18:43:52 +0530

    custom user model ready to go

--- users/tests.py
+++ users/tests.py
@@ -33,7 +33,7 @@ class UsersManagersTests(TestCase):
         self.assertEqual(admin_user.email, 'super@user.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
-        self.asserTrue(admin_user.is_superuser)
+        self.assertTrue(admin_user.is_superuser)
         try:
             self.assertIsNone(admin_user.username)
         except AttributeError:
