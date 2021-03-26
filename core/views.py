4389ecbb71e6dc6548c468801cc09bddfde7b582
Rishabh <jrishabh252@gmail.com>
2021-03-26 16:53:41 +0530

    remove from cart created. :)

--- core/views.py
+++ core/views.py
@@ -31,7 +31,28 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['post'], )
     def remove_from_cart(self, request, pk=None):
-        return Response({'product': 'removed from cart'})
+        product_id = request.data.get('product', None)
+        if product_id is None:
+            return Response({'product': 'Product is required'})
+        try:
+            order = Order.objects.get(pk=pk)
+            product = Product.objects.get(pk=product_id)
+            if order is None:
+                return Response({'order_id': 'Order id is invalid'})
+            if order.products.filter(product=product).exists():
+                order_product = order.products.filter(product=product)[0]
+                if order_product.quantity == 1:
+                    OrderProduct.objects.filter(pk=order_product.id).delete()
+                else:
+                    order_product.quantity -= 1
+                    order_product.save()
+                return Response({'product': 'Product removed successfully'})
+
+            else:
+                return Response({'product_id': 'Product id is invalid'})
+        except Exception as e:
+            print(e)
+            return Response({'order_id': 'Order id is invalid'})
 
     @action(detail=True, methods=['post'],)
     def add_to_cart(self, request, pk=None):
@@ -40,7 +61,6 @@ class OrderViewSet(viewsets.ModelViewSet):
             return Response({'product': 'Product is required'})
         try:
             product = Product.objects.get(pk=product_id)
-
             order_query_set = Order.objects.all().filter(owner=request.user, ordered=False)
             if order_query_set.exists():
                 order = order_query_set[0]
@@ -60,8 +80,8 @@ class OrderViewSet(viewsets.ModelViewSet):
             print(e)
             return Response({'product': 'Invalid product id provided'})
 
-    def perform_create(self, serializer):
-        serializers.save(owner=self.request.user)
+    # def perform_create(self, serializer):
+    #     serializers.save(owner=self.request.user)
 
     def list(self, request):
         queryset = Order.objects.all().filter(owner=request.user.pk)
